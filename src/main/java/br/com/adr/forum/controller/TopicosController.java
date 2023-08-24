0514b5ed0b95b708c7b6e731d7a008d4b5d69d7b
Adryan Claro <adryan.gustavomarques@gmail.com>
2023-08-24 00:11:46 -0300

    Project finalization Added error handling, data deletion and update methods, h2 database configuration.

--- src/main/java/br/com/adr/forum/controller/TopicosController.java
+++ src/main/java/br/com/adr/forum/controller/TopicosController.java
@@ -2,13 +2,15 @@ package br.com.adr.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.adr.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.adr.forum.controller.dto.TopicoDto;
+import br.com.adr.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.adr.forum.controller.form.TopicoForm;
 import br.com.adr.forum.modelo.Topico;
 import br.com.adr.forum.repository.CursoRepository;
 import br.com.adr.forum.repository.TopicoRepository;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/topicos")
@@ -47,6 +52,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		
 		Topico topico = form.converter(cursoRepository);
@@ -57,11 +63,39 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
 		
-		Topico topico = topicoRepository.getOne(id);
-		return new DetalhesDoTopicoDto(topico); 
+		Optional<Topico> topico = topicoRepository.findById(id);
+		if(topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get())); 
+		}
+		return ResponseEntity.notFound().build();
 		
 	}
 	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+		
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if(optional.isPresent()) {
+			Topico topico = form.atualizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		
+		Optional<Topico> optional = topicoRepository.findById(id);
+		if(optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+		
+	}
 }
